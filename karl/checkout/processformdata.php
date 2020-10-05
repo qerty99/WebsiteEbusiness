7a82c2dfe6aba117fe42ab0688cfa4d42b25374e
Aysha <aysha.espedido@gmail.com>
2020-10-05 22:35:40 +1100

    Checkout updated

---
+++ karl/checkout/processformdata.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Process Form Data</title>
+</head>
+<body>
+    <h1>Process Form Data</h1>
+
+    <?php
+        $email=htmlspecialchars($POST["email_address"]);
+
+        echo "Email: " . $name . "<br />";
+    ?>
+</body>
+</html>
--- karl/js/simplecartjs-config.js
+++ karl/js/simplecartjs-config.js
@@ -2,12 +2,12 @@ simpleCart({
     checkout: {
         type: "SendForm" ,
         method: "POST",
-        url: "http://saturn.csit.rmit.edu.au/∼e46281/inte2047/processFormData.php"
+        url: "http://saturn.csit.rmit.edu.au/~e46281/inte2047/processFormData.php"
     },
 });
 
 simpleCart.bind('beforeCheckout', function(data){
-    data.email = document.getElementById("email-address").value;
+    data.email_address = document.getElementById("email_address").value;
     data.first_name = document.getElementById("fname").value;
     data.last_name = document.getElementById("lname").value;
     data.street1 = document.getElementById("street_address1").value;
@@ -16,6 +16,18 @@ simpleCart.bind('beforeCheckout', function(data){
     data.postcode = document.getElementById("postal_code").value;
     data.state = document.getElementById("state").value;
     data.country = document.getElementById("country").value;
+
+    /*Get radio buttons */
+    data.std_shipping = document.getElementById("std_shipping").checked;
+    data.express_shipping = document.getElementById("express_shipping").checked;
+
+    data.cc_mastercard = document.getElementById("cc_mastercard").checked;
+    data.cc_visa = document.getElementById("cc_visa").checked;
+    data.cc_amex = document.getElementById("cc_amex").checked;
+
+    data.ccname = document.getElementById("credit_card_name").value;
+    data.ccnumber = document.getElementById("credit_card_number").value;
+    data.ccsecuritycode = document.getElementById("credit_card_security_code").value;
 
 
 });
