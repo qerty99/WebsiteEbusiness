c77f646fd26e2fa05935b5240ef0cb41f71c03d7
Aysha <aysha.espedido@gmail.com>
2020-10-07 03:33:49 +1100

    Script fixes

--- karl/alumnium_case_page.php
+++ karl/alumnium_case_page.php
@@ -7,7 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="../js/simpleCart.js"></script>
+    <script src="../js/simplecartjs-config.js"></script>
     <!-- Title  -->
     <title>The Suited Switch | 60% Aluminum Case</title>
 
