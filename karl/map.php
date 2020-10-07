f560d6cebdff52bab29ea340670da2c75ca48ed4
Raiyan <Raiyan.ramb@gmail.com>
2020-10-07 11:27:31 +1100

    Re-added map.php

---
+++ karl/map.php
@@ -0,0 +1,172 @@
+<?php include('Login.php'); ?>
+<html lang="en">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="../js/simpleCart.js"></script>
+    <script src="../js/simplecartjs-config.js"></script>
+    <!-- Title  -->
+    <title>The Suited Switch | Site Map</title>
+    <!-- Favicon  -->
+    <link rel="icon" href="img/core-img/favicon.ico">
+    <!-- Core Style CSS -->
+    <link rel="stylesheet" href="css/core-style.css">
+    <link rel="stylesheet" href="style.css">
+    <!-- Responsive CSS -->
+    <link href="css/responsive.css" rel="stylesheet">
+  </head>
+  <body>
+    <?php include('menu.php'); ?>
+    <!-- ****** Header Area End ****** -->
+    <section class="custom-keyboard">
+      <div class="section-heading text-center">
+        <h2>Site Map</h2>
+      </div>
+      <a href="index.php">Home</a><br>
+      <a href="aboutus.php">About Us</a><br>
+      <a href="#">Store</a><br>
+      <a href="cases-shop.php">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; Cases</a><br>
+      <a href="#">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+        60%</a><br>
+      <a href="alumnium_case_page.php">&nbsp;&nbsp; &nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp; 60%
+        Aluminum Case</a><br>
+      <a href="60plastic_case.php">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; 60% Plastic
+        Case</a><br>
+      <a href="60acrylic_case.php">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; 60% Acrylic
+        Case</a><br>
+      <a href="60wooden.php">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; 60% Wooden Case</a><br>
+      <a href="#">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; 80%</a><br>
+      <a href="80aluminum_case.php">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; 80% Aluminum
+        Case</a><br>
+      <a href="#">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; 100%</a><br>
+      <a href="100aluminum_case.php">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; 100% Aluminum
+        Case</a><br>
+      <a href="keycaps.php">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; Keycaps</a><br>
+      <a href="cherry_keycaps.php">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; MX Cherry
+        Keycaps</a><br>
+      <a href="sakeycaps.php">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; SA Keycaps</a><br>
+      <a href="pcb.php">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; PCB</a><br>
+      <a href="60pcb.php">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; 60%</a><br>
+      <a href="60HS.php">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; 60% Hot-Swap</a><br>
+      <a href="80pcb.php">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp; 80%</a><br>
+      <a href="80HS.php">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp; 80% Hot-Swap</a><br>
+      <a href="100pcb.php">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; 100%</a><br>
+      <a href="100HS.php">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+        100% Hot-Swap</a><br>
+      <a href="custom.php">Custom builds</a><br>
+      <a href="contact_us.php">Contact Us</a><br>
+      <a href="cart.php">Cart</a><br>
+      <a href="checkout.php">Checkout</a><br>
+      <a href="logout.php">Logout</a><br>
+      <a href="Login.php">Login</a><br>
+    </section>
+    <!-- ****** Footer Area Start ****** -->
+    <footer class="footer_area">
+      <div class="container">
+        <div class="row">
+          <!-- Single Footer Area Start -->
+          <div class="col-12 col-md-6 col-lg-3">
+            <div class="single_footer_area">
+              <div class="footer-logo"> <img src="img/core-img/logo.png" alt="">
+              </div>
+              <div class="copywrite_text d-flex align-items-center">
+                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                  Copyright ©
+                  <script>document.write(new Date().getFullYear());</script> All
+                  rights reserved | This template is made with by <a href="https://colorlib.com"
+
+                    target="_blank">Colorlib</a>
+                  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+              </div>
+            </div>
+          </div>
+          <!-- Single Footer Area Start -->
+          <div class="col-12 col-sm-6 col-md-3 col-lg-2">
+            <div class="single_footer_area">
+              <ul class="footer_widget_menu">
+                <li><a href="map.php">Site Map</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Faq</a></li>
+                <li><a href="#">Returns</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+          <!-- Single Footer Area Start -->
+          <div class="col-12 col-sm-6 col-md-3 col-lg-2">
+            <div class="single_footer_area">
+              <ul class="footer_widget_menu">
+                <li><a href="#">My Account</a></li>
+                <li><a href="#">Shipping</a></li>
+                <li><a href="#">Our Policies</a></li>
+                <li><a href="#">Afiliates</a></li>
+              </ul>
+            </div>
+          </div>
+          <!-- Single Footer Area Start -->
+          <div class="col-12 col-lg-5">
+            <div class="single_footer_area">
+              <div class="footer_heading mb-30">
+                <h6>Subscribe to our newsletter</h6>
+              </div>
+              <div class="subscribtion_form">
+                <form action="#" method="post"> <input name="mail" class="mail"
+
+                    placeholder="Your email here" type="email"> <button type="submit"
+
+                    class="submit">Subscribe</button> </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="line"></div>
+        <!-- Footer Bottom Area Start -->
+        <div class="footer_bottom_area">
+          <div class="row">
+            <div class="col-12">
+              <div class="footer_social_area text-center"> <a href="#"><i class="fa fa-pinterest"
+
+                    aria-hidden="true"></i></a> <a href="#"><i class="fa fa-facebook"
+
+                    aria-hidden="true"></i></a> <a href="#"><i class="fa fa-twitter"
+
+                    aria-hidden="true"></i></a> <a href="#"><i class="fa fa-linkedin"
+
+                    aria-hidden="true"></i></a> </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- ****** Footer Area End ****** -->
+    <!-- /.wrapper end -->
+    <!-- jQuery (Necessary for All JavaScript Plugins) -->
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Plugins js -->
+    <script src="js/plugins.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
+  </body>
+</html>
